2c5e82509f997009a8bfe5577db45774f4f5cde5
Carlo Gilmar <carlogilmar12@gmail.com>
2020-06-01 23:55:21 -0500

    Adding dep for yaml parser

--- readme_generator/mix.exs
+++ readme_generator/mix.exs
@@ -21,8 +21,7 @@ defmodule ReadmeGenerator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:yaml_elixir, "~> 2.4"}
     ]
   end
 end
