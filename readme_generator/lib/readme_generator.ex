fbf4044710e68d0cdd40ef9c0402043908db12d3
Carlo Gilmar <carlogilmar12@gmail.com>
2020-06-01 23:56:34 -0500

    Adding @spec

--- readme_generator/lib/readme_generator.ex
+++ readme_generator/lib/readme_generator.ex
@@ -1,18 +1,4 @@
 defmodule ReadmeGenerator do
-  @moduledoc """
-  Documentation for `ReadmeGenerator`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReadmeGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
+
