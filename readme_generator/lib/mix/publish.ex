c174b9945c1bd8951ed001175b6b10cabb49ea5a
Carlo Gilmar <carlogilmar12@gmail.com>
2020-10-07 18:41:49 -0500

    Starting hackney

--- readme_generator/lib/mix/publish.ex
+++ readme_generator/lib/mix/publish.ex
@@ -2,6 +2,7 @@ defmodule Mix.Tasks.Publish do
   use Mix.Task
 
   def run(_) do
+    Application.ensure_all_started(:hackney)
     ReadmeGenerator.Publish.post_dev_to()
     IO.puts "Dev To Post Published!..."
   end
